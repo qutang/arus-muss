a4934dd49e642c27a2ebd1609c5405964de30f64
qutang <qu.tang@outlook.com>
2020-11-05 21:09:08 -0500

    test: Add test case for  model prediction task

--- arus_muss/tests/test_model.py
+++ arus_muss/tests/test_model.py
@@ -8,12 +8,13 @@ from sklearn.pipeline import Pipeline
 import arus
 
 from ..har_model import MUSSHARModel
+from ..import cli
 
 
 class TestMUSSHARModel:
     @pytest.mark.parametrize('placements', [['DW'], ['DW', 'DA']])
     @pytest.mark.parametrize('task_name', ['ACTIVITY_VALIDATED', 'POSTURE_VALIDATED', 'INTENSITY'])
-    def test_mussharmodel(self, placements, task_name):
+    def test_train_mussharmodel(self, placements, task_name):
         spades_lab = arus.ds.MHDataset(
             path=arus.ds.get_dataset_path('spades_lab'),
             name='spades_lab', input_type=arus.ds.InputType.MHEALTH_FORMAT)
@@ -28,7 +29,7 @@ class TestMUSSHARModel:
 
         model.load_dataset(spades_1_ds)
 
-        model.compute_features()
+        model.compute_features(pids=['SPADES_1'], n_cores=1)
         model.compute_class_set(
             task_names=['ACTIVITY_VALIDATED', 'POSTURE_VALIDATED', 'INTENSITY'])
 
@@ -38,3 +39,12 @@ class TestMUSSHARModel:
         assert model.train_perf['acc'] > 0.9
 
         shutil.rmtree(model.get_processed_path(), ignore_errors=True)
+
+    @pytest.mark.parametrize('task_name', ['ACTIVITY_VALIDATED', 'POSTURE_VALIDATED', 'INTENSITY'])
+    def test_predict_mussharmodel(self, task_name):
+        model_path = cli._get_model_path(
+            placements=['NDW'], pids='ALL', target=task_name, model_path=None)
+        predict_df = cli.predict_on_files(model_path,
+                                          test_files=['test.signaligner.csv'], placements=[
+                                              'NDW'], srs=[80], file_format='SIGNALIGNER')
+        assert predict_df.shape[0] == 136
